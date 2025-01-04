ab2af2369242b1ae0873d7ffa6c2efa0b2b4d513
v.popova <violipopova@gmail.com>
2025-01-05 00:14:58 +0200

    feat(contact screen); adjustments to contact screen

--- app/contact.tsx
+++ app/contact.tsx
@@ -1,109 +1,95 @@
-import { StyleSheet, Image, Platform } from 'react-native';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+import { Fontisto } from '@expo/vector-icons';
+import { StyleSheet, SafeAreaView, View, Text, Appearance } from 'react-native';
+import { Link } from 'expo-router';
 
-export default function TabTwoScreen() {
+import { Colors } from '@/constants/Colors';
+
+export default function ContactScreen() {
+  const colorScheme = Appearance.getColorScheme() || 'light';
+
+  const theme = colorScheme === 'dark' ? Colors.dark : Colors.light;
+
+  const imgColor = colorScheme === 'dark' ? 'papayawhip' : '#333';
+
+  const styles = createStyles(theme, colorScheme);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
+
+    <SafeAreaView style={styles.container}>
+
+      <View style={styles.imgContainer}>
+        <Fontisto
+          name="coffeescript"
+          size={250}
+          color={imgColor}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Contact Us</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/contact.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+      </View>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Coffee Shop</Text>
+
+        <View style={styles.textView}>
+          <Text style={styles.text}>
+            <Text>555 Coffee Lane</Text>{'\n'}
+            <Text>Kansas City, KS  55555-1234</Text>
+          </Text>
+        </View>
+
+        <View style={styles.textView}>
+          <Text style={styles.text}>
+            Phone:{'\n'}
+            <Link href="tel:5555555555" style={styles.link}>555-555-5555</Link>{'\n'}
+            or{' '}
+            <Link href="sms:5555555555" style={styles.link}>Click Here to Text!</Link>
+          </Text>
+        </View>
+
+        <View style={styles.textView}>
+          <Text style={styles.text}>
+            Hours:{'\n'}
+            <Text>Open 6am to 4pm daily.</Text>{'\n'}
+          </Text>
+        </View>
+
+      </View>
+
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
-});
+function createStyles(theme: { background: string; text: string }, colorScheme: 'light' | 'dark' | null) {
+  return StyleSheet.create({
+    container: {
+      backgroundColor: theme.background,
+      paddingTop: 0,
+      flexGrow: 1
+    },
+    imgContainer: {
+      backgroundColor: colorScheme === 'dark' ? '#353636' : '#D0D0D0',
+      height: 250,
+    },
+    textContainer: {
+      backgroundColor: theme.background,
+      padding: 12,
+    },
+    title: {
+      color: theme.text,
+      fontSize: 24,
+      fontWeight: 'bold',
+      lineHeight: 32,
+      marginBottom: 10,
+    },
+    textView: {
+      marginBottom: 10,
+    },
+    text: {
+      color: theme.text,
+      fontSize: 16,
+      lineHeight: 24,
+    },
+    link: {
+      textDecorationLine: 'underline',
+    }
+  });
+}
